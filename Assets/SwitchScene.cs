cbc65b91ff943acfa3db9fcd75638b19c5b3baaf
Zakaria-ElGhoul <30044@ma-web.nl>
2022-02-15 16:37:41 +0100

    Monke 2

--- Assets/SwitchScene.cs
+++ Assets/SwitchScene.cs
@@ -1,14 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.Events;
 
-public class Click : MonoBehaviour
+public class SwitchScene : MonoBehaviour
 {
     public UnityEvent OnClickActivate;
 
     private void OnMouseDown()
     {
         OnClickActivate.Invoke();
+    }
+    public void ChangeScene(int index)
+    {
+        SceneManager.LoadScene(index);
     }
 }
